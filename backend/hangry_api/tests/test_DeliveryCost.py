da7aaaf5eb637049a437c8d871c8fefff008a8f7
yesahek <yesahek@gmail.com>
2026-02-24 20:27:01 +0300

    fix automated unit tests

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -37,4 +37,4 @@ def test_LittleItems():
   cost = Delivery.calculate(order, del_dist)
   #Assert
   # TODO: replace the pass with an assert to test the value returned.
-  assert cost ==2.51
+  assert cost ==2.50
