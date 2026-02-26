6a991043603ddb94b5cbc99d8c452b63b8af076a
yesahek <yesahek@gmail.com>
2026-02-26 09:50:17 +0300

    add functional test

--- frontend/src/pages/Order/Order.test.js
+++ frontend/src/pages/Order/Order.test.js
@@ -63,7 +63,7 @@ describe('Test Order', () => {
     );
     //Assert: replace the return true.
     await waitFor(() => {
-      return true;
+      expect(screen.getAllByText('$5.00')).toHaveLength(1);
     });
   });
 });
