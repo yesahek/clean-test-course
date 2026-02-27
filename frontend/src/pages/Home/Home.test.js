22e65ded7e210eaecc8c72dbd055eb9755348352
yesahek <yesahek@gmail.com>
2026-02-27 11:50:39 +0300

    add test integration

--- frontend/src/pages/Home/Home.test.js
+++ frontend/src/pages/Home/Home.test.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 import Home from '.';
 
 describe('Test Home', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   test('Test Render', async () => {
     //Arrange: Setup the mock API
     //Listen for any GET requests using the axios module
@@ -41,6 +45,17 @@ describe('Test Home', () => {
     //Act: Call the Home page
     render(<Home />);
 
+    //Assert: Check the values in the rendered Home page.
+    //There should be 2 categories as defined in the mock response above
+    expect(await screen.findAllByTestId(/category-item/i)).toHaveLength(2);
+    //The word Appeateasers should be in there as defined in the mock response above.
+    expect(await screen.findByText('Appeteasers')).toBeInTheDocument();
+  });
+
+
+  test('Test Integration Render', async () => {
+    //Act: Call the Home page
+    render(<Home />);
     //Assert: Check the values in the rendered Home page.
     //There should be 2 categories as defined in the mock response above
     expect(await screen.findAllByTestId(/category-item/i)).toHaveLength(2);
